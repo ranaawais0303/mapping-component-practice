353a4129f3d77c53a2241dea2dc370b22cbf6bcb
Rana_awais <z.manzoor@pioneerlogics.com>
2022-06-07 06:12:00 -0700

    emoji app with map

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import emoji from "./Emoji";
+import Entry from "./Entry";
+
+const createEmoji = function (emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      meaning={emojiTerm.meaning}
+    />
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emoji.map(createEmoji)}
+        {/* <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div> */}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
